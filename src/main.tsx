83ad39ea349d2b2040b14b7d0d4bdf30dd4a5f58
huuthangcv0612 <huuthang.cv0612@gmail.com>
2026-09-18 18:02:12 +0700

    add gg

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import App from './App.tsx'
 import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider
+      clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}
+    >
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>,
 )
